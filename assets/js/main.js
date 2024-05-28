b93330a0fdd140fcc678a763fd85475153c4aecf
Nymul Islam Moon <nymulislam.webappick@gmail.com>
2024-05-28 16:33:18 +0600

    Most replayed done

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const searchButton = document.getElementById('search-button'),
     searchClose = document.getElementById('search-close'),
     searchContent = document.getElementById('search-content')
 
-/*===== MENU SHOW =====*/
+/*===== SEARCH SHOW =====*/
 /* Validate if constant exists */
 if(searchButton) {
     searchButton.addEventListener('click', () =>{
@@ -11,7 +11,7 @@ if(searchButton) {
     })
 }
 
-/*===== MENU HIDDEN =====*/
+/*===== SEARCH HIDDEN =====*/
 /* Validate if constant exists */
 if(searchClose) {
     searchClose.addEventListener('click', () =>{
@@ -20,12 +20,48 @@ if(searchClose) {
 }
 
 /*=============== LOGIN ===============*/
+const loginButton = document.getElementById('login-button'),
+    loginClose = document.getElementById('login-close'),
+    loginContent = document.getElementById('login-content')
 
+/*===== LOGIN SHOW =====*/
+/* Validate if constant exists */
+if(loginButton) {
+    loginButton.addEventListener('click', () =>{
+        loginContent.classList.add('show-login')
+    })
+}
+
+/*===== LOGIN HIDDEN =====*/
+/* Validate if constant exists */
+if(loginClose) {
+    loginClose.addEventListener('click', () =>{
+        loginContent.classList.remove('show-login')
+    })
+}
 
 /*=============== ADD SHADOW HEADER ===============*/
 
 
 /*=============== HOME SWIPER ===============*/
+let swiperHome = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: -24,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        1220: {
+            spaceBetween: -32,
+        }
+    }
+});
 
 
 /*=============== FEATURED SWIPER ===============*/
