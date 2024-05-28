8072aa50fb4e8f871df54d1aa5c35f61d2bee662
Nymul Islam Moon <towkir197islam@gmail.com>
2024-05-28 21:53:46 +0600

    Section Featured done

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,7 +71,25 @@ let swiperHome = new Swiper('.home__swiper', {
 
 
 /*=============== FEATURED SWIPER ===============*/
+let swiperFeatured = new Swiper('.featured__swiper', {
+    loop: true,
+    spaceBetween: -16,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
 
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        1150: {
+            spaceBetween: 4,
+            centeredSlides: false,
+        }
+    }
+});
 
 /*=============== NEW SWIPER ===============*/
 
