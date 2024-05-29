944f68b910d27131926ca8279545f3a56c26dbb8
Nymul Islam Moon <nymulislam.webappick@gmail.com>
2024-05-29 12:24:19 +0600

    Section Testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,7 +105,20 @@ let swiperNew = new Swiper('.new__swiper', {
 });
 
 /*=============== TESTIMONIAL SWIPER ===============*/
+let swiperTestimonial = new Swiper('.testimonial__swiper', {
+    loop: true,
+    spaceBetween: 16,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
 
+    breakpoints: {
+        1150: {
+            spaceBetween: 3,
+            centeredSlides: false,
+        }
+    }
+});
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
