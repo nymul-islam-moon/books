13ee24da8c99080af85a4d439bac9936862424c8
Nymul Islam Moon <nymulislam.webappick@gmail.com>
2024-05-29 14:37:16 +0600

    Scroll Bar scrollup

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,7 +121,14 @@ let swiperTestimonial = new Swiper('.testimonial__swiper', {
 });
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add  the show-scroll class to the a tag with the scroll__up class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
 
+window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
